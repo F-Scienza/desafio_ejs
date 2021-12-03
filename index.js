506bd50dc0ae4bb4fb3c0a7e958dc2fe357cd5f1
F-Scienza <cufa1905@gmail.com>
2021-12-03 11:30:59 -0300

    products is undefined on index ejs

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const app = express()
 
 //  establecemos el motor de plantilla ejs
 //  y la carpeta que contiene los .ejs
-app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({
+    extended : true
+}))
 
+const prodcutos = []
 
 //  establecemos las rutas y el 404
 //  le pasamos un objeto a los archivos
 app.get("/", (req, res) => {
-    res.render("index", { titulo: "inicio ejs" })
+    res.render("index", { prodcutos })
 })  
 
-app.get("/personas", (req, res) => {
-    res.render("personas", { titulo: "personas ejs" })
+app.post("/productos", (req, res) => {
+    prodcutos.push(req.body)
+    res.render("index", { prodcutos })
 })  
 
 app.use((req, res, next ) =>{
