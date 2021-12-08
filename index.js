f2dc53092007d57a070e09d977fe05596ad8729c
F-Scienza <cufa1905@gmail.com>
2021-12-08 14:34:14 -0300

    agregar estilos, aplicacion funcionando

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
-const express = require ("express")
-const app = express()
+const express = require('express');
+const Contenedor = require('./contenedor');
+const multer = require('multer')
+const { Router } = express;
 
-//  establecemos el motor de plantilla ejs
-//  y la carpeta que contiene los .ejs
-app.set("views", __dirname + "/views")
-app.set("view engine", "ejs")
+const app = express();
 app.use(express.json())
 app.use(express.urlencoded({
-    extended : true
+	extended: true
 }))
 
-const prodcutos = []
+// 	seteamos la carpeta views y el engine 
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
-//  establecemos las rutas y el 404
-//  le pasamos un objeto a los archivos
-app.get("/", (req, res) => {
-    res.render("index", { prodcutos })
-})  
+const router = Router();
+const productos = new Contenedor(__dirname + "/data/productos.json")
+productos.init()
 
-app.post("/productos", (req, res) => {
-    prodcutos.push(req.body)
-    res.render("index", { prodcutos })
-})  
+// seteamos el file con multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage })
+app.use('/files', express.static('uploads'))
 
-app.use((req, res, next ) =>{
-    res.status(404).render("404", {tutulo: "pagina 404"})
+router.get("/", (req, res)=>{
+    return res.json(productos.productList);
+})
+router.get("/:id", async (req, res) => {
+    let id = req.params.id //leemos lo que pasó por url el usuario
+	return res.json(await productos.getById(id));
+});	
+
+//	usamos multer para guardar las imagenes 
+router.post("/", upload.single('thumbnail') ,async(req, res) => {
+	let obj = req.body;	
+	obj.thumbnail = "/files/" + req.file.filename 
+	await productos.addProduct(obj) // usamos el metodo save
+	return res.redirect("/productList"); //redireccionamos a lista
+});
+
+router.put("/:id", (req, res) => {
+	let obj = req.body;
+	let id = req.params.id;
+	return res.json(productos.update(id, obj));
+});
+
+app.use("/api/productos/", router)
+
+//	mostramos el formulario
+app.get("/", (req, res)=> {
+    return res.render('form')
 })
 
-app.listen(3000)
+// 	publicamos la carpeta upload
+app.use(express.static('uploads'))
+
+app.get("/productList", (req, res)=> {
+    return res.render('productList.ejs', {
+		productos: productos.productList
+	})
+})
+
+app.listen(8080)
